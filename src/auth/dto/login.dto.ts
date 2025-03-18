72bac843ad17dc2e5ace2fe61b18fb176ee88594
Matheus Teixeira Câmara Mourão <matheusmouraotc@gmail.com>
2025-03-18 17:04:46 -0300

    Refactor: Auth DTO's

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,12 @@
+import { Trim } from 'class-sanitizer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  @Trim()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,16 @@
+import { Trim } from 'class-sanitizer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
+  @Trim()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Trim()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
